c06ccdeb2024ab1048b18f457eb30d4d34956abe
Mahmudsana06 <sana06eee@gmail.com>
2021-08-02 15:34:32 +0600

    change the name of the plane

--- testing.py
+++ testing.py
@@ -1,1 +1,25 @@
-print("Hello world")
+class Flight():
+    def __init__(self,capacity):
+        self.capacity=capacity
+        self.passenger=[]
+    
+    def add_passenger(self,name):
+        if not self.empty_seat():
+            return False
+        self.passenger.append(name)
+        return True
+    
+    def empty_seat(self):
+        return self.capacity - len(self.passenger)
+
+flight=Flight(3)
+
+people=["A","B","C","D","E"]
+
+for i in people:
+    success = flight.add_passenger(i)
+
+    if success:
+        print(f"Adding {i} is successful.")
+    else:
+        print(f"{i} cannot be added.")
---
+++ testing2.py
@@ -0,0 +1,1 @@
+print("This is a program on adding the passengers based on the number of empty seats in the airplane.")
